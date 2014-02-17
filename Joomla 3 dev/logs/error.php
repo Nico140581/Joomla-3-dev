0d455822ac2aadd1eef97480e92f45fc0a0c2462
nicolas <ndeslandes@hotmail.fr>
2014-02-17 21:12:36 +0100

    Joomla 3 standard

---
+++ Joomla 3 dev/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-02-16 17:06:18 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-02-16T17:06:18+00:00	INFO	joomlafailure	Le mot de passe ne correspond pas au nom d'utilisateur, ou vous n'avez pas encore de compte.
